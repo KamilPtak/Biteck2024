a27f2dcd83e7ce20a0af4c0a2d6fa0bad08037c6
Marcin Malcher <marcin.malcher@10g.pl>
2024-01-20 19:09:47 +0100

    Added constraints on allowed motor positions

--- mainPico/motors.py
+++ mainPico/motors.py
@@ -3,6 +3,7 @@ from servo import Servo
 
 stepperH = Stepper(0,1,2,3,-1)
 servoV = Servo(28)
+servoV.move(90)
 
 stepsPerDegree=64/5.625
 oldPos=0
@@ -19,10 +20,18 @@ def calculateStepperDiff(newPos):
 def callback(topic, msg):
     print("In motor callback, topic: ", topic)
     if topic == b'pico/mot/hor':
-        steps = calculateStepperDiff(float(msg))
+        requestedPosition = float(msg)
+        if requestedPosition > 180 or requestedPosition < -180:
+            print("Requested V position out of range, ignoring:", requestedPosition)
+            return
+        steps = calculateStepperDiff(requestedPosition)
         print("Moving horizontally by ", steps)
         stepperH.run(steps)
+        
     elif topic == b'pico/mot/vert':
         pos = 90-float(msg)
+        if pos > 45 or pos < -45:
+            print('Requested H position out of range, ignoring: ', pos)
+            return
         print("Moving vertically to ", pos)
         servoV.move(pos)
