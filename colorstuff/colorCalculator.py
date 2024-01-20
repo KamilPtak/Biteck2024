567bb6d101b3fe3da056c2e9dc6c28983f98f90a
Marcin Malcher <marcin.malcher@10g.pl>
2024-01-20 23:10:05 +0100

    Add stuff related to turning on lights

---
+++ colorstuff/colorCalculator.py
@@ -0,0 +1,2 @@
+def emotionToRgb(emotions):
+    return {'r':100,'g':50,'b':200}
---
+++ colorstuff/colorSender.py
@@ -0,0 +1,20 @@
+import tinytuya
+import mySecrets
+DEVICE_ID=mySecrets.DEVICE_ID
+IP_ADDR=mySecrets.IP_ADDR
+LOCAL_KEY=mySecrets.LOCAL_KEY
+
+d = tinytuya.BulbDevice(DEVICE_ID,IP_ADDR, LOCAL_KEY)
+d.set_version(3.3)
+# data = d.status()
+# print('Device status: %r' % data)
+# resp = d.set_colour(200,120,50, False)
+# print(resp)
+# d.set_value(20, False)
+
+def setLightsColor(rgb):
+    d.set_colour(rgb['r'], rgb['g'], rgb['b'], False)
+
+
+def setLightOn(on=True):
+    d.set_value(20, on)
